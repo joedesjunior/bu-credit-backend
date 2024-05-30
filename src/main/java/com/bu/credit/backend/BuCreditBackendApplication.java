25272fe23ba31c8e99d3979cda61a7a3519d61ec
Joedes Junior <joedes.junior-ext@anbima.com.br>
2024-05-29 22:00:55 -0300

    Criação da estrutura inicial do projeto

---
+++ src/main/java/com/bu/credit/backend/BuCreditBackendApplication.java
@@ -0,0 +1,13 @@
+package com.bu.credit.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuCreditBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuCreditBackendApplication.class, args);
+	}
+
+}
